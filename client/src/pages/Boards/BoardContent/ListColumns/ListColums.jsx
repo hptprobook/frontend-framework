89398569dc0e1955d8ddb6a834e796f73bd24506
Phan Thanh Hoa <hptprobook@gmail.com>
2024-03-10 16:00:11 +0700

    Complete Drag Drop column

--- client/src/pages/Boards/BoardContent/ListColumns/ListColums.jsx
+++ client/src/pages/Boards/BoardContent/ListColumns/ListColums.jsx
@@ -2,51 +2,55 @@ import Box from '@mui/material/Box';
 import Column from './Column/Column';
 import Button from '@mui/material/Button';
 import PlaylistAddIcon from '@mui/icons-material/PlaylistAdd';
+import { SortableContext, horizontalListSortingStrategy } from '@dnd-kit/sortable';
 
 function ListColums({ columns }) {
+  /* SortableContext yêu cầu items là một dạng mảng dữ liệu thông thường chứ không phải dạng mảng object */
   return (
-    <Box
-      sx={{
-        bgcolor: 'inherit',
-        width: '100%',
-        display: 'flex',
-        height: '100%',
-        overflowX: 'auto',
-        overflowY: 'hidden',
-        '&::-webkit-scrollbar-track': {
-          m: 2,
-        },
-      }}
-    >
-      {columns?.map((column) => (
-        <Column key={column?._id} column={column} />
-      ))}
-
-      {/* Box Add new column */}
+    <SortableContext items={columns?.map((c) => c._id)} strategy={horizontalListSortingStrategy}>
       <Box
         sx={{
-          minWidth: '300px',
-          maxWidth: '300px',
-          bgcolor: '#ffffff3d',
-          mx: 2,
-          borderRadius: '6px',
-          height: 'fit-content',
+          bgcolor: 'inherit',
+          width: '100%',
+          display: 'flex',
+          height: '100%',
+          overflowX: 'auto',
+          overflowY: 'hidden',
+          '&::-webkit-scrollbar-track': {
+            m: 2,
+          },
         }}
       >
-        <Button
-          startIcon={<PlaylistAddIcon />}
+        {columns?.map((column) => (
+          <Column key={column?._id} column={column} />
+        ))}
+
+        {/* Box Add new column */}
+        <Box
           sx={{
-            color: '#fff',
-            width: '100%',
-            justifyContent: 'flex-start',
-            pl: 4,
-            py: 1.2,
+            minWidth: '300px',
+            maxWidth: '300px',
+            bgcolor: '#ffffff3d',
+            mx: 2,
+            borderRadius: '6px',
+            height: 'fit-content',
           }}
         >
-          Add new column
-        </Button>
+          <Button
+            startIcon={<PlaylistAddIcon />}
+            sx={{
+              color: '#fff',
+              width: '100%',
+              justifyContent: 'flex-start',
+              pl: 4,
+              py: 1.2,
+            }}
+          >
+            Add new column
+          </Button>
+        </Box>
       </Box>
-    </Box>
+    </SortableContext>
   );
 }
 
