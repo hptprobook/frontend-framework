5250f8756c5ae7e441cd7b5f786a2b1fd5b81a77
Phan Thanh Hoa <hptprobook@gmail.com>
2024-03-13 10:38:08 +0700

    Base routes completed

--- server/src/routes/v1/index.js
+++ server/src/routes/v1/index.js
@@ -1,1 +1,16 @@
-//
+import express from 'express';
+import { StatusCodes } from 'http-status-codes';
+import { boardRoutes } from './boardRoutes';
+
+const Router = express.Router();
+
+Router.get('/status', (req, res) => {
+  res.status(StatusCodes.OK).json({
+    message: 'APIs v1 are ready to use.',
+  });
+});
+
+/* Board APIs */
+Router.use('/boards', boardRoutes);
+
+export const APIs_V1 = Router;
