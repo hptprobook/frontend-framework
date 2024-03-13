9ef07dc871e7398550fb5a876d3d7aa398454c7f
Phan Thanh Hoa <hptprobook@gmail.com>
2024-03-13 09:57:08 +0700

    environment variable

--- server/src/config/mongodb.js
+++ server/src/config/mongodb.js
@@ -1,1 +1,30 @@
-//
+import { MongoClient, ServerApiVersion } from 'mongodb';
+import { env } from '~/config/environment';
+
+/* Khởi tạo một đối tượng ban đầu là null */
+let trelloDatabaseInstance = null;
+
+// Khởi tạo một đối tượng client để connect tới mongoDB
+const client = new MongoClient(env.MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+// Kết nối tới database
+export const CONNECT_DB = async () => {
+  await client.connect();
+
+  trelloDatabaseInstance = client.db(env.DATABASE_NAME);
+};
+
+export const GET_DB = () => {
+  if (!trelloDatabaseInstance) throw new Error('Must connect to Database first');
+  return trelloDatabaseInstance;
+};
+
+export const CLOSE_DB = async () => {
+  await client.close();
+};
--- server/src/server.js
+++ server/src/server.js
@@ -1,30 +1,35 @@
+/* eslint-disable no-console */
+
 import express from 'express';
-import { mapOrder } from '~/utils/sorts.js';
+import exitHook from 'async-exit-hook';
+import { CONNECT_DB, GET_DB, CLOSE_DB } from '~/config/mongodb';
+import { env } from '~/config/environment';
 
-const app = express();
+const START_SERVER = () => {
+  const app = express();
 
-const hostname = 'localhost';
-const port = 8017;
+  app.get('/', async (req, res) => {
+    console.log(await GET_DB().listCollections().toArray());
+    res.end('<h1>Hello World!</h1><hr>');
+  });
 
-app.get('/', (req, res) => {
-  // Test Absolute import mapOrder
-  console.log(
-    mapOrder(
-      [
-        { id: 'id-1', name: 'One' },
-        { id: 'id-2', name: 'Two' },
-        { id: 'id-3', name: 'Three' },
-        { id: 'id-4', name: 'Four' },
-        { id: 'id-5', name: 'Five' },
-      ],
-      ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
-      'id'
-    )
-  );
-  res.end('<h1>Hello World!</h1><hr>');
-});
+  app.listen(env.APP_PORT, env.APP_HOST, () => {
+    console.log(`Server is running at http://${env.APP_HOST}:${env.APP_PORT}/`);
+  });
 
-app.listen(port, hostname, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Hello World, I am running at http://${hostname}:${port}/`);
-});
+  exitHook(() => {
+    CLOSE_DB();
+    console.log('Disconnected from MongoDB Atlas');
+  });
+};
+
+(async () => {
+  try {
+    await CONNECT_DB();
+    console.log('Connect to MongoDB Atlas successfully');
+    START_SERVER();
+  } catch (error) {
+    console.log(error);
+    process.exit(0);
+  }
+})();
