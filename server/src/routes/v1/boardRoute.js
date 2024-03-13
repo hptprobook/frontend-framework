16916e326a0824645b5c743d7b67738dd2e8beae
Phan Thanh Hoa <hptprobook@gmail.com>
2024-03-13 15:51:57 +0700

    Complete validation backend, errorhandling

--- server/src/routes/v1/boardRoute.js
+++ server/src/routes/v1/boardRoute.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import { StatusCodes } from 'http-status-codes';
+import { boardValidation } from '~/validations/boardValidation';
+import { boardController } from '~/controllers/boardController';
 
 const Router = express.Router();
 
@@ -7,8 +9,6 @@ Router.route('/')
   .get((req, res) => {
     res.status(StatusCodes.OK).json({ message: 'Get all boards' });
   })
-  .post((req, res) => {
-    res.status(StatusCodes.CREATED).json({ message: 'Create new board' });
-  });
+  .post(boardValidation.createNew, boardController.createNew);
 
-export const boardRoutes = Router;
+export const boardRoute = Router;
--- server/src/server.js
+++ server/src/server.js
@@ -1,15 +1,22 @@
-/* eslint-disable no-console */
+/* eslint-disable */
 
 import express from 'express';
 import exitHook from 'async-exit-hook';
 import { CONNECT_DB, CLOSE_DB } from '~/config/mongodb';
 import { env } from '~/config/environment';
 import { APIs_V1 } from '~/routes/v1';
+import { errorHandlingMiddleware } from './middlewares/errorHandlingMiddleware';
 
 const START_SERVER = () => {
   const app = express();
 
+  app.use(express.json());
+
+  // Use APIs v1
   app.use('/v1', APIs_V1);
+
+  // Middleware xử lý lỗi tập trung
+  app.use(errorHandlingMiddleware);
 
   app.listen(env.APP_PORT, env.APP_HOST, () => {
     console.log(`Server is running at http://${env.APP_HOST}:${env.APP_PORT}/`);
