73c089db03ec85ed296b9c17017e80e4e3ffba58
Phan Thanh Hoa <hptprobook@gmail.com>
2024-04-18 15:45:35 +0700

    Update constants.js

--- server/src/utils/constants.js
+++ server/src/utils/constants.js
@@ -1,8 +1,8 @@
 export const WHITELIST_DOMAINS = [
   'http://localhost:5173',
   'http://localhost:5174',
-  'https://frontend-framework-sigma.vercel.app/',
-  'hpt-trello.vercel.app',
+  'https://frontend-framework-sigma.vercel.app',
+  'https://hpt-trello.vercel.app',
 ];
 
 export const BOARD_TYPES = ['public', 'private'];
