a440000584cdcfe980f981815e41c3e9cf063d8e
Phan Thanh Hoa <hptprobook@gmail.com>
2024-04-18 15:25:36 +0700

    Update server.js

--- server/src/server.js
+++ server/src/server.js
@@ -26,9 +26,17 @@ const START_SERVER = () => {
   // Middleware xử lý lỗi tập trung
   app.use(errorHandlingMiddleware);
 
-  app.listen(env.APP_PORT, env.APP_HOST, () => {
-    console.log(`Server is running at http://${env.APP_HOST}:${env.APP_PORT}/`);
-  });
+  if (env.BUILD_MODE === 'production') {
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running at ${process.env.PORT}`);
+    });
+  } else {
+    app.listen(env.APP_PORT, env.APP_HOST, () => {
+      console.log(
+        `Server is running at http://${env.APP_HOST}:${env.APP_PORT}/`
+      );
+    });
+  }
 
   exitHook(() => {
     CLOSE_DB();
